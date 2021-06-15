3847c7210e0a5701dbfedf477a4435375ab90557
pouriaRahmani <pouriarahmani@yahoo.com>
2021-06-15 15:42:36 +0430

    corporate template

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".toggle-menu");
+const list = document.querySelector("nav ul");
+btn.addEventListener("click", () => {
+    list.classList.toggle("active");
+    btn.classList.toggle("active");
+});
